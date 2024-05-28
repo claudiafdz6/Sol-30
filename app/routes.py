0dfd9377d87eb0a8baf0f536cc56ff0c96f80baf
Claudia Fernandez <claudia.fernandez-XT@solutions30italia.it>
2024-05-28 11:06:38 +0200

    changes in routes and index

--- app/routes.py
+++ app/routes.py
@@ -17,19 +17,22 @@ def add_ticket():
         id_task = request.form['id_task']
         note = request.form.get('note')
         tag = request.form.get('tag')
-        
-                
+        data_apertura = request.form['data_apertura']
+        data_chiusura= request.form['data_chiusura']
+
         new_ticket = TicketSupervisor(
             id=id,
             utente_apertura=utente_apertura,
             utente_segnalato=utente_segnalato,
             id_task=id_task,
             note=note,
-            tag=tag
+            tag=tag,
+            data_apertura=data_apertura,
+            data_chiusura=data_chiusura
         )
         
         db.session.add(new_ticket)
         db.session.commit()
         
-        print('Ticket has been correctly added')
+        print('Ticket has been added')
         return redirect(url_for('index'))
